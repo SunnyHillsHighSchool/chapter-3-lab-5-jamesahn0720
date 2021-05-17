4f4c81c79044c6a6d4a9884cac0ed1824de92b63
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 23:37:13 -0700

    Update PostFix.java

--- PostFix.java
+++ PostFix.java
@@ -14,24 +14,64 @@ public class PostFix
 
  public PostFix()
  {
+   stack = new Stack<Double>();
  }
 
  public PostFix(String exp)
  {
+  stack = new Stack<Double>();
+       setExpression(exp);
  }
 
  public void setExpression(String exp)
  {
+  this.expression = exp;
  }
 
  public double calc(double one, double two, char op)
  {
-  return 0.0;
+  double result = 0.0;
+       switch(op) 
+                { 
+                    case '+': 
+                    result = one + two; 
+                    break; 
+                      
+                    case '-': 
+                    result = two - one; 
+                    break; 
+                      
+                    case '/': 
+                    result = two/one; 
+                    break; 
+                      
+                    case '*': 
+                    result = one * two; 
+                    break; 
+              } 
+        return result;
  }
 
  public void solve()
  {
+   String value[] = this.expression.split(" ");
+       for(int i = 0; i < value.length; i++) {
+           if(value[i].equals("+") || value[i].equals("-") ||value[i].equals("*") || value[i].equals("/")) {
+               double result = calc(stack.pop(), stack.pop(), value[i].charAt(0));
+               stack.push(result);
+           }
+           else {
+               stack.push(Double.parseDouble(value[i]));
+           }
+       }
  }
 
- //add a toString
+  public String toString() {
+       if(!stack.empty()) {
+           return(this.expression + " = " + Double.toString(stack.pop()));
+       }
+       else {
+           return null;
+       }
+   }
 }
